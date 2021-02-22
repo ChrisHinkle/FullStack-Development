f03a143f8d0872ecd06057a6c757d19d4cde1a1a
Christopher Hinkle <christopher.hinkle@snhu.edu>
2021-02-21 23:13:10 -0500

    Create app.js

---
+++ Christopher Hinkle Milestone Four/app.js
@@ -0,0 +1,37 @@
+// MONGODB PW: PASSWORD HERE
+// MONGODB CONNECTION: mongodb+srv://DATABSE HERE/<dbname>?retryWrites=true&w=majority
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+mongoose.connect('mongodb+srv://ENTER DATABASE INFO HERE')
+	.then(() => {
+		console.log('Successfully connected to MongoDB Atlas');
+	})
+	.catch((error) => {
+		console.log('Unable to connect to MongoDB Atlas');
+		console.error(error);
+	});
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
